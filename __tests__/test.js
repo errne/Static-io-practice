2813fd88611526e6b1a07c4ca76cfd7fbf716443
Ernest <ernest.fraiman@gmail.com>
2024-03-09 11:04:17 +0000

    Update test.js

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,3 +1,6 @@
+const { updateNumber, resetNumbers } = require('../test-script.js');
+
+// Mock the DOM environment using JSDOM
 const { JSDOM } = require('jsdom');
 const dom = new JSDOM(`
   <!DOCTYPE html>
@@ -17,23 +20,15 @@ const dom = new JSDOM(`
 `);
 global.document = dom.window.document;
 
+// Mock the getElementById function
 global.document.getElementById = jest.fn((id) => {
   return {
     disabled: false,
-    innerText: '',
+    innerText: '?',
     id: id,
   };
 });
 
-test('updateNumber updates the displayed number for Team 1', () => {
-    updateNumber(1);
-    expect(document.getElementById('number1').innerText).not.toBe('?');
-});
-
-test('updateNumber updates the displayed number for Team 2', () => {
-    updateNumber(2);
-    expect(document.getElementById('number2').innerText).not.toBe('?');
-});
 
 test('resetNumbers resets the displayed number for Team 1', () => {
     resetNumbers();
@@ -44,42 +39,3 @@ test('resetNumbers resets the displayed number for Team 2', () => {
     resetNumbers();
     expect(document.getElementById('number2').innerText).toBe('?');
 });
-
-
-function updateNumber(buttonNumber) {
-    let buttonId = "button" + buttonNumber;
-
-    if (document.getElementById(buttonId).disabled) {
-        return;
-    }
-
-    if (buttonNumber === 1) {
-        global.document.getElementById = jest.fn(("number1") => {
-          return {
-            disabled: false,
-            innerText: '66',
-            id: id,
-          };
-        });
-    } else if (buttonNumber === 2) {
-        global.document.getElementById = jest.fn(("number2") => {
-          return {
-            disabled: false,
-            innerText: '76',
-            id: id,
-          };
-        });
-    }
-
-    document.getElementById(buttonId).disabled = true;
-}
-
-function resetNumbers() {
-     global.document.getElementById = jest.fn((id) => {
-    return {
-      disabled: false,
-      innerText: '?',
-      id: id,
-    };
-  });
-}
